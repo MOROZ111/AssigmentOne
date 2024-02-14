38762adb5df42e7ac82b9056dc7b167236829a92
Vadym Dulibskyy <you@example.com>
2024-02-14 10:09:32 -0500

    project created

---
+++ Vadym_Dulibskyy_A1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Vadym_Dulibskyy_A1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Vadym_Dulibskyy_A1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Vadym_Dulibskyy_A1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
